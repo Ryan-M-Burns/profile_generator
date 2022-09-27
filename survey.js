cc7a7ab2eeafab9e2bc9db025dd8e99b28dd86d9
Ryan Matthew Burns <ryan.m.burns123@gmail.com>
2022-09-27 23:01:23 +0000

    working survey with a nice profile return

---
+++ survey.js
@@ -0,0 +1,30 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What is your name? Nicknames are acceptable :): ', (ans1) => {
+  
+  rl.question("What's an activity you like doing?: ", (ans2) => {
+  
+    rl.question("What do you listen to while doing that?: ", (ans3) => {
+  
+      rl.question("Which meal is your favourite (eg: dinner, brunch, etc.): ", (ans4) => {
+  
+        rl.question("What's your favourite thing to eat for that meal?: ", (ans5) => {
+  
+          rl.question("Which sport is your absolute favourite?; ", (ans6) => {
+  
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at!: ", (ans7) => {
+              
+              console.log(`\n${ans1} loves listening to ${ans3} while ${ans2}, devouring ${ans5} for ${ans4}, ${ans6} over any other sport, and is amazing at ${ans7}.\n`);
+
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
